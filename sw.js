89d2c25c9e4f5cb92414450314cc7d69f459c410
PlanetoStudios <carvalhomarandubay@gmail.com>
2026-01-02 12:01:31 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,23 @@
-const CACHE_NAME = "evolux-v1";
+const CACHE_NAME = "evolux-v2";
 
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache =>
-      cache.addAll([
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
         "./",
         "./index.html",
         "./manifest.json",
         "./icon-192.png",
         "./icon-512.png"
-      ])
-    )
+      ]);
+    })
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
